c58c851800fab85ee1ebcaaffb760a9b37eca3b0
Ryan Sullivan <ryansully96@googlemail.com>
2022-08-08 19:49:08 +0000

    Initial commit

---
+++ build.rs
@@ -0,0 +1,5 @@
+extern crate embed_resource;
+
+fn main() {
+    embed_resource::compile("martel-printer-programmer.rc");
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+
+    println!("Ello World");
+
+    loop {}
+}
